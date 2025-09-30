fd240b2383935fbd3bc4c7147a3738f88c00081d
ArturMatukiwa <artur.matukiwa@sptech.school>
2025-09-29 22:14:19 -0300

    adicao nome da fila no consumer

--- src/main/java/studiozero/service/email/infrastructure/consumer/EmailConsumer.java
+++ src/main/java/studiozero/service/email/infrastructure/consumer/EmailConsumer.java
@@ -1,6 +1,7 @@
 package studiozero.service.email.infrastructure.consumer;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import studiozero.service.email.application.usecases.SendDailyEmailUseCase;
 
@@ -12,7 +13,7 @@ public class EmailConsumer {
         this.sendDailyEmailUseCase = sendDailyEmailUseCase;
     }
 
-    @RabbitListener
+    @RabbitListener(queues = "${broker.daily-email-queue}")
     public void consumeEvent(ConsumeEmailEventDto eventDto) {
         sendDailyEmailUseCase.execute(eventDto);
     }
