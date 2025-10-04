42379628c857ec9e7f40a9bdd7ea27577db935f6
Gustavo Gil <gustavo.gil@sptech.school>
2025-10-04 18:26:36 -0300

    ajustando config do sender

--- src/main/java/studiozero/service/email/infrastructure/consumer/EmailConsumer.java
+++ src/main/java/studiozero/service/email/infrastructure/consumer/EmailConsumer.java
@@ -2,12 +2,14 @@ package studiozero.service.email.infrastructure.consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import studiozero.service.email.application.usecases.SendDailyEmailUseCase;
 
 @Component
+@EnableRabbit
 public class EmailConsumer {
     private final Logger log = LoggerFactory.getLogger(EmailConsumer.class);
     private SendDailyEmailUseCase sendDailyEmailUseCase;
